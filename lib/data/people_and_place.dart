65cf475b082e943fc9687476ab91a79ef2d8232d
Dianagit168 <dyna30432@gmail.com>
2024-05-24 03:51:52 +0700

    Complete search screen

---
+++ lib/data/people_and_place.dart
@@ -0,0 +1,121 @@
+import 'package:clone_photo_app/model/albums_cate_model.dart';
+
+List<AlbumsCateModel> people = [
+  AlbumsCateModel(
+      cateName: '',
+      qty: '1',
+      thumnail:
+          'https://imageio.forbes.com/specials-images/imageserve/5f5be112e7f395dc08ef8e58/Lionel-Messi-celebrating-scoring-a-goal-in-the-2019-20-UEFA-Champions-League/1960x0.jpg?format=jpg&width=960'),
+  AlbumsCateModel(
+      cateName: '',
+      qty: '2',
+      thumnail:
+          'https://images.immediate.co.uk/production/volatile/sites/3/2024/01/Vinicius-Jr-6c9ba9a.jpg?quality=90&fit=700,466'),
+  AlbumsCateModel(
+      cateName: '',
+      qty: '3',
+      thumnail:
+          'https://images.immediate.co.uk/production/volatile/sites/3/2024/01/Harry-Kane-02a7c74.jpg?quality=90&fit=700,466'),
+  AlbumsCateModel(
+      cateName: '',
+      qty: '4',
+      thumnail:
+          'https://assets.goal.com/images/v3/blt0564bd9c19909e11/Ronny_cover(1).jpg'),
+  AlbumsCateModel(
+      cateName: '',
+      qty: '5',
+      thumnail:
+          'https://www.shape.com/thmb/bccHlJ2oWE76dB8_6Yu4y5s6dJk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/neymar_1-b19698ead32b4c55983e3bad0a218402.jpg'),
+  AlbumsCateModel(
+      cateName: '',
+      qty: '1',
+      thumnail:
+          'https://imageio.forbes.com/specials-images/imageserve/5f5be112e7f395dc08ef8e58/Lionel-Messi-celebrating-scoring-a-goal-in-the-2019-20-UEFA-Champions-League/1960x0.jpg?format=jpg&width=960'),
+  AlbumsCateModel(
+      cateName: '',
+      qty: '2',
+      thumnail:
+          'https://images.immediate.co.uk/production/volatile/sites/3/2024/01/Vinicius-Jr-6c9ba9a.jpg?quality=90&fit=700,466'),
+  AlbumsCateModel(
+      cateName: '',
+      qty: '3',
+      thumnail:
+          'https://images.immediate.co.uk/production/volatile/sites/3/2024/01/Harry-Kane-02a7c74.jpg?quality=90&fit=700,466'),
+  AlbumsCateModel(
+      cateName: '',
+      qty: '4',
+      thumnail:
+          'https://assets.goal.com/images/v3/blt0564bd9c19909e11/Ronny_cover(1).jpg'),
+  AlbumsCateModel(
+      cateName: '',
+      qty: '5',
+      thumnail:
+          'https://www.shape.com/thmb/bccHlJ2oWE76dB8_6Yu4y5s6dJk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/neymar_1-b19698ead32b4c55983e3bad0a218402.jpg'),
+];
+
+List<AlbumsCateModel> place = [
+  AlbumsCateModel(
+    cateName: 'Phom Penh',
+    qty: '1',
+    thumnail: 'https://cdn.hswstatic.com/gif/maps.jpg',
+  ),
+  AlbumsCateModel(
+    cateName: 'Kaoh Soutin',
+    qty: '2',
+    thumnail: 'https://cdn.hswstatic.com/gif/maps.jpg',
+  ),
+  AlbumsCateModel(
+    cateName: 'Srey Santhor',
+    qty: '3',
+    thumnail: 'https://cdn.hswstatic.com/gif/maps.jpg',
+  ),
+  AlbumsCateModel(
+    cateName: 'Kang Meas',
+    qty: '4',
+    thumnail: 'https://cdn.hswstatic.com/gif/maps.jpg',
+  )
+];
+List<AlbumsCateModel> categories = [
+  AlbumsCateModel(
+    cateName: 'Cats',
+    qty: '1',
+    thumnail:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTL7tiX3ThP9Y196M-Ymphfi42EHQ9BfC4TU2cUlVAusCRyP0IdTgYUHCLN4d-baAyFbE&usqp=CAU',
+  ),
+  AlbumsCateModel(
+    cateName: 'Animals',
+    qty: '2',
+    thumnail:
+        'https://images.pexels.com/photos/47547/squirrel-animal-cute-rodents-47547.jpeg?cs=srgb&dl=pexels-pixabay-47547.jpg&fm=jpg',
+  ),
+  AlbumsCateModel(
+    cateName: 'Art',
+    qty: '3',
+    thumnail:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9P8VvhlFipcWos5pYnBhi_cWfBdLn0J4_Cut391hArg&s',
+  ),
+  AlbumsCateModel(
+    cateName: 'Food',
+    qty: '4',
+    thumnail:
+        'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505',
+  ),
+  AlbumsCateModel(
+    cateName: 'Babies',
+    qty: '4',
+    thumnail:
+        'https://s3.gomedia.ws/wp-content/uploads/sites/72/2021/09/17191023/cute-baby-scaled.jpeg',
+  ),
+  AlbumsCateModel(
+    cateName: 'Motorcycles',
+    qty: '4',
+    thumnail:
+        'https://lh4.googleusercontent.com/proxy/Vg3m7DFiOUmNCQp58LwB2TLO2vFJdTrJLaWFxdOz4wCkwdCHj1Nk0QS8iItc4wm40FqrdZI_JFWR0KJTgRL5X73SjCRpwfUV-PxIlBE4fWBVe2xfbqBeADlvaspQDXhMhlFUaf0-zOdZMdq1tU-3GaYc5mYU1h-Mlr09inIsvHi5c0V8ftnnq3uDc5gUbIcvPthBFs4qJgAjIraSRM_825c',
+  ),
+  AlbumsCateModel(
+    cateName: 'Car',
+    qty: '4',
+    thumnail:
+        'https://images.netdirector.co.uk/gforces-auto/image/upload/w_343,h_257,dpr_2.0,q_auto,c_fill,f_auto,fl_lossy/auto-client/4f3a94f309517d480173272c101fe224/thumbnail_mobile.jpg',
+  )
+];
--- lib/screen/SubScreen/search_screen.dart
+++ lib/screen/SubScreen/search_screen.dart
@@ -1,3 +1,5 @@
+import 'package:clone_photo_app/data/people_and_place.dart';
+import 'package:clone_photo_app/model/albums_cate_model.dart';
 import 'package:flutter/material.dart';
 
 class SeachScreen extends StatelessWidget {
@@ -6,64 +8,103 @@ class SeachScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Container(
-        //color: Colors.pink,
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text('data'),
-            Container(
-              decoration: BoxDecoration(
-                  color: Colors.grey, borderRadius: BorderRadius.circular(16)),
-              child: const TextField(
-                decoration: InputDecoration(
-                    contentPadding: EdgeInsets.symmetric(vertical: 8),
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide.none,
-                    ),
-                    prefixIcon: Icon(
-                      Icons.search,
-                      size: 32,
-                    ),
-                    hintText: 'Photos, People, Places'),
+      child: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Container(
+          //color: Colors.pink,
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Search',
+                style: TextStyle(fontSize: 34, fontWeight: FontWeight.bold),
               ),
-            ),
-            listWidgetOnSearch(context, null, BoxShape.circle, 'People')
-          ],
+              Padding(
+                padding: const EdgeInsets.only(top: 4, bottom: 16),
+                child: Container(
+                  height: 38,
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 216, 215, 215),
+                    borderRadius: BorderRadius.circular(14),
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.zero,
+                      border: const OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                      ),
+                      prefixIcon: Icon(
+                        Icons.search,
+                        size: 28,
+                        color: Colors.grey[600],
+                      ),
+                      hintText: 'Photos, People, Places...',
+                      hintStyle: TextStyle(
+                        color: Colors.grey[600],
+                        fontSize: 18,
+                        fontWeight: FontWeight.normal,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              listWidgetOnSearch(context, BoxShape.circle, 'People', people),
+              listWidgetOnSearch(context, BoxShape.rectangle, 'Places', place),
+              listWidgetOnSearch(
+                  context, BoxShape.rectangle, 'Categories', categories),
+              groups(context, 'Groups', categories)
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
-Widget listWidgetOnSearch(
-    BuildContext context, dynamic dataList, BoxShape shape, String contain) {
+Widget listWidgetOnSearch(BuildContext context, BoxShape shape, String contain,
+    List<AlbumsCateModel> data) {
   return Column(
     crossAxisAlignment: CrossAxisAlignment.start,
     children: [
-      Text(contain),
+      Padding(
+        padding: const EdgeInsets.only(bottom: 4, top: 8),
+        child: Text(
+          contain,
+          style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+        ),
+      ),
       SizedBox(
         //color: Colors.pink,
-        height: 170,
+        height: 160,
         child: ListView.builder(
-          // shrinkWrap: true,
           scrollDirection: Axis.horizontal,
-          itemCount: 30,
+          itemCount: data.length,
           itemBuilder: (context, index) {
             return Column(
-              //crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Container(
-                  margin: const EdgeInsets.only(right: 8),
+                  margin: const EdgeInsets.only(right: 10),
                   decoration: BoxDecoration(
-                    color: Colors.amber,
+                    borderRadius: shape == BoxShape.rectangle
+                        ? BorderRadius.circular(4)
+                        : null,
                     shape: shape,
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage(data[index].thumnail),
+                    ),
                   ),
-                  height: 100,
-                  width: 100,
+                  height: 110,
+                  width: 110,
                 ),
-                const Text('data')
+                Padding(
+                  padding: const EdgeInsets.only(top: 4),
+                  child: Text(
+                    data[index].cateName,
+                  ),
+                )
               ],
             );
           },
@@ -71,7 +112,104 @@ Widget listWidgetOnSearch(
       ),
       const Divider(
         height: 1,
-        thickness: 1.5,
+        thickness: 0.4,
+        color: Colors.grey,
+      ),
+    ],
+  );
+}
+
+Widget groups(
+    BuildContext context, String content, List<AlbumsCateModel> data) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Padding(
+        padding: const EdgeInsets.only(bottom: 4, top: 8),
+        child: Text(
+          content,
+          style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+        ),
+      ),
+      SizedBox(
+        height: 110,
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: data.length,
+          itemBuilder: (context, index) {
+            return Container(
+              margin: const EdgeInsets.only(right: 10),
+              child: Row(children: [
+                Container(
+                  height: 110,
+                  width: 54,
+                  decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(4),
+                        bottomLeft: Radius.circular(4),
+                      ),
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(data[index].thumnail),
+                      )),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(left: 1),
+                  height: 110,
+                  width: 54,
+                  decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: const BorderRadius.only(
+                        topRight: Radius.circular(4),
+                        bottomRight: Radius.circular(4),
+                      ),
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(people[index].thumnail),
+                      )),
+                ),
+              ]),
+            );
+          },
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.only(top: 32),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text(
+              'Recently Searched',
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            ),
+            IconButton(
+              padding: EdgeInsets.zero,
+              onPressed: () {},
+              icon: const Text(
+                'Clear',
+                style: TextStyle(color: Colors.blueAccent, fontSize: 18),
+              ),
+            )
+          ],
+        ),
+      ),
+      const Divider(
+        height: 0.5,
+        thickness: 0.4,
+        color: Colors.grey,
+      ),
+      IconButton(
+        padding: EdgeInsets.zero,
+        onPressed: () {},
+        icon: const Text(
+          'Favorites',
+          style: TextStyle(color: Colors.blueAccent, fontSize: 20),
+        ),
+      ),
+      const Divider(
+        height: 0.5,
+        thickness: 0.4,
         color: Colors.grey,
       ),
     ],
